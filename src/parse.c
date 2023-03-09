dfdf2ce272caf69f1083006fee92912b633b8a09
vtrevisa <vi.trevi.11@gmail.com>
2023-03-09 15:02:23 -0300

    added some tolkens for cmd and args

--- src/parse.c
+++ src/parse.c
@@ -6,7 +6,7 @@
 /*   By: vtrevisa <vtrevisa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 17:21:05 by vtrevisa          #+#    #+#             */
-/*   Updated: 2023/03/07 18:40:35 by vtrevisa         ###   ########.fr       */
+/*   Updated: 2023/03/09 15:00:44 by vtrevisa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ char	*space_to_nonprint(char *str)
 	index = 0;
 	while (str[index])
 	{
-		if (str[index] == '\'')
+		if (str[index] == '\'' || str[index] == '\"')
 			flag *= -1;
 		if (str[index] == ' ' && flag == 1)
 			str[index] = -1;
@@ -46,7 +46,7 @@ int	count_splitted(t_data *data)
 	return (splits);
 }
 
-int	test_cmd(t_data *data, char *line_splitted)
+int	iscmd(t_data *data, char *line_splitted)
 {
 	char	*tmp;
 	char	*cmd;
@@ -69,25 +69,43 @@ int	test_cmd(t_data *data, char *line_splitted)
 	return (0);
 }
 
+int	isarg(t_data *data, char *line_splitted)
+{
+	int index;
+
+	index = 0;
+	if (line_splitted[0] == '-')
+		return (1);
+	else
+		return (0);
+}
+
 void	tolkenizer(t_data *data)
 {
 	int	index;
+	int	max_index;
 
-	index = count_splitted(data);
-	while (index >= 0)
+	max_index = count_splitted(data);
+	data->parsed_class = malloc(sizeof (int) * max_index);
+	ft_bzero(data->parsed_class, max_index-1);
+	index = 0;
+	while (index <= max_index)
 	{
-		if(test_cmd(data, data->line_splitted[index]))
+		if(iscmd(data, data->line_splitted[index]))
 		{
-			data->parsed_class[index] == 1;
-			ft_printf("%s: is a cmd\n", data->line_splitted[index]);
+			data->parsed_class[index] = 1;
+			ft_printf("%s: is a cmd: %d\n", data->line_splitted[index], data->parsed_class[index]);
+		}
+		else if (isarg(data, data->line_splitted[index]))
+		{
+			data->parsed_class[index] = 2;
+			ft_printf("%s: is arg: %d\n", data->line_splitted[index], data->parsed_class[index]);
 		}
 		else
-			ft_printf("%s: not a cmd\n", data->line_splitted[index]);
-		//else if (/*isarg*/)
-		//	data->parsed_class[index] == 2;
+			ft_printf("%s: not a cmd nor arg: %d\n", data->line_splitted[index], data->parsed_class[index]);
 		//else if (/*issimble*/)
-	//		data->parsed_class[index] == 3;
-		index--;
+	//		data->parsed_class[index] = 3;
+		index++;
 	}
 }
 
