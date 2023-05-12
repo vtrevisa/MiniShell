4279fe1c89dcae8cc6e3458462eaaa90608f3452
roger <romachad@users.noreply.github.com>
2023-05-12 03:44:56 +0200

    Improved parsing of redir, still initial state

--- src/parse_redir.c
+++ src/parse_redir.c
@@ -6,17 +6,22 @@
 /*   By: romachad <romachad@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 04:46:03 by romachad          #+#    #+#             */
-/*   Updated: 2023/05/11 06:37:20 by romachad         ###   ########.fr       */
+/*   Updated: 2023/05/12 03:41:48 by romachad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../Include/minishell.h"
 
-char	**output_redir(char **parsed, t_parser *p)
+char	**remove_redir(char **parsed, t_parser *p, char *redir_type)
 {
 	int	count;
 	char	**copy;
 
+	if (ft_strlen(redir_type) == ft_strlen(parsed[p->i]) && parsed[p->i + 1])
+	{
+		p->i++;
+		parsed = remove_redir(parsed, p, redir_type);
+	}
 	count = 0;
 	while (parsed[count])
 		count++;
@@ -26,29 +31,30 @@ char	**output_redir(char **parsed, t_parser *p)
 	ft_memcpy(copy + p->i, parsed + p->i + 1, (count - p->i - 1) * sizeof(parsed[0]));
 	free(parsed[p->i]);
 	free(parsed);
+	p->i--;
 	return (copy);
 }
 
 
-char	**parse_redir(char **str)
+char	**parse_redir(char **parsed)
 {
 	t_parser	p;
-	char	**parsed;
+	//char	**parsed;
 
-	parsed = copy_double_str(str);
+	//parsed = copy_double_str(str);
 	p.i = -1;
 	p.j = 0;
-	while (str[++p.i])
+	while (parsed[++p.i])
 	{
-		if (ft_strncmp(str[p.i], ">>", 2) == 0)
+		if (ft_strncmp(parsed[p.i], ">>", 2) == 0)
 			//append_change_output()
 			printf("append output redir\n");
-		else if (ft_strncmp(str[p.i], ">", 1) == 0)
-			parsed = output_redir(parsed, &p);
+		else if (ft_strncmp(parsed[p.i], ">", 1) == 0)
+			parsed = remove_redir(parsed, &p, ">");
 			//printf("output redir\n");
-		else if (ft_strncmp(str[p.i], "<<", 2) == 0)
+		else if (ft_strncmp(parsed[p.i], "<<", 2) == 0)
 			printf("here-doc\n");
-		else if (ft_strncmp(str[p.i], "<", 1) == 0)
+		else if (ft_strncmp(parsed[p.i], "<", 1) == 0)
 			printf("input file");
 		p.j++;
 	}
